9b5def968f6b9d9b30a64e3252b75d1bb7550ed0
Tan Zhi Wei <zhiwei@outlook.sg>
2016-08-19 20:51:41 +0800

    Add project files.

---
+++ DIP 101/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DIP_101
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(279, 219);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(274, 651);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 866);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ DIP 101/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace DIP_101
+{
+    public partial class Form1 : Form
+    {
+        public delegate void AddListView1(string text);
+        ThreadStart threadStart;
+        Thread readThread;
+
+        SerialPort _serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Getting port available
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+                listView1.Items.Add(port);
+
+
+            threadStart = new ThreadStart(Read_Arduino);
+            readThread = new Thread(threadStart);
+
+
+             _serialPort = new SerialPort(ports[0]);
+
+            _serialPort.BaudRate = 9600;
+
+            _serialPort.ReadTimeout = 5000;
+            _serialPort.WriteTimeout = 5000;
+            
+            _serialPort.StopBits = 0;
+            _serialPort.Open();
+
+            listView1.Items.Add(_serialPort.BaudRate.ToString());
+
+            readThread.Start();
+
+
+        }
+        private void Read_Arduino(){
+            AddListView1 DelUpdateUITextBox = new AddListView1(AddListView);
+            while (true)
+            {
+                try
+                {
+                    listView1.BeginInvoke(DelUpdateUITextBox, _serialPort.ReadLine());
+                }
+                catch (System.InvalidOperationException)
+                {
+
+                }
+                catch (System.IO.IOException)
+                {
+                    //TO DO INSERT ERROR MESSAGE SOMEWHERE
+                    
+                } 
+                catch (System.TimeoutException)
+                {
+                    //TIME OUT ERROR
+                }
+
+            }
+        }
+        public void AddListView(string text)
+        {
+            this.listView1.Items.Add(text);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+    }
+}
